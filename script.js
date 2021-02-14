4f81258c0f42bc4ed6b29dce213fd29a82b2a977
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-02-14 17:41:34 -0500

    Added functionality to load the songs when the play class is active. Application is functioning as intended.

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ const progress = document.getElementById("progress");
 const progressContainer = document.getElementById("progress-container");
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
+
+// Array of song titles.
+const songs = [
+  "Blue Boi",
+  "By The Pool",
+  "Chill Day",
+  "Doing Just Fine",
+  "I Found Me",
+  "In My Clouds",
+  "Overjoyed",
+  "This Feeling",
+  "Visions",
+  "Warm Nights",
+];
+
+// Keeps track of song.
+let songIndex = 2;
+
+// Function to load song details into DOM initially.
+loadSong(songs[songIndex]);
+
+// Function to update song details (image/title/music).
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/covers/${song}.jpg`;
+}

9213e95a5020ebea2a2d3d4cd1b7d76040355473
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-02-14 17:23:16 -0500

    Brought in all the DOM elements for the javascript component.

---
+++ script.js
@@ -0,0 +1,12 @@
+// DOM Elements.
+const musicContainer = document.getElementById("music-container");
+
+const playButton = document.getElementById("play");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
